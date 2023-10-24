27e7bf03b43e97624591b207ea9d60bf018fb6b5
Eric Dallo <ericdallo06@hotmail.com>
2023-10-24 10:57:32 -0300

    Check for repl connected

--- src/main/clojure/com/github/clojure_repl/intellij/action/eval_last_sexp.clj
+++ src/main/clojure/com/github/clojure_repl/intellij/action/eval_last_sexp.clj
@@ -3,6 +3,7 @@
    :name com.github.clojure_repl.intellij.action.EvalLastSexp
    :extends com.intellij.openapi.actionSystem.AnAction)
   (:require
+   [com.github.clojure-repl.intellij.db :as db]
    [com.github.clojure-repl.intellij.editor :as editor]
    [com.github.clojure-repl.intellij.nrepl :as nrepl]
    [com.github.clojure-repl.intellij.parser :as parser]
@@ -16,13 +17,15 @@
 (set! *warn-on-reflection* true)
 
 (defn -actionPerformed [_ ^AnActionEvent event]
-  (let [editor ^Editor (.getData event CommonDataKeys/EDITOR_EVEN_IF_INACTIVE)
-        [row col] (editor/editor->cursor-position editor)
-        text (.getText (.getDocument editor))
-        root-zloc (z/of-string text)
-        zloc (parser/find-at-pos root-zloc row col)
-        code (z/string zloc)
-        {:keys [value err]} (nrepl/eval {:code code})]
-    (if err
-      (.showErrorHint (HintManager/getInstance) editor (str "=> " err) (HintManager/RIGHT))
-      (.showInformationHint (HintManager/getInstance) editor (str "=> " (or value "nil")) (HintManager/RIGHT)))))
+  (if (-> @db/db* :current-nrepl :session-id)
+    (let [editor ^Editor (.getData event CommonDataKeys/EDITOR_EVEN_IF_INACTIVE)
+          [row col] (editor/editor->cursor-position editor)
+          text (.getText (.getDocument editor))
+          root-zloc (z/of-string text)
+          zloc (parser/find-at-pos root-zloc row col)
+          code (z/string zloc)
+          {:keys [value err]} (nrepl/eval {:code code})]
+      (if err
+        (.showErrorHint (HintManager/getInstance) editor (str "=> " err) (HintManager/RIGHT))
+        (.showInformationHint (HintManager/getInstance) editor (str "=> " (or value "nil")) (HintManager/RIGHT))))
+    (.showErrorHint (HintManager/getInstance) (.getData event CommonDataKeys/EDITOR_EVEN_IF_INACTIVE) "No REPL connected" (HintManager/RIGHT))))
