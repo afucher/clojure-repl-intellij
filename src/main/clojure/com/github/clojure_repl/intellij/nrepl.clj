8e47a2910556894d05578eda9fcbb840eb2d2977
Eric Dallo <ericdallo06@hotmail.com>
2024-04-13 15:30:09 -0300

    Fix print failing test

--- src/main/clojure/com/github/clojure_repl/intellij/nrepl.clj
+++ src/main/clojure/com/github/clojure_repl/intellij/nrepl.clj
@@ -1,7 +1,9 @@
 (ns com.github.clojure-repl.intellij.nrepl
   (:refer-clojure :exclude [eval load-file])
   (:require
+   [com.github.clojure-repl.intellij.config :as config]
    [com.github.clojure-repl.intellij.db :as db]
+   [com.github.ericdallo.clj4intellij.logger :as logger]
    [nrepl.core :as nrepl.core])
   (:import
    [com.intellij.openapi.editor Editor]
@@ -12,15 +14,20 @@
 (set! *warn-on-reflection* true)
 
 (defn ^:private send-message [project message]
-  (with-open [conn ^FnTransport (nrepl.core/connect
-                                 :host (db/get-in project [:current-nrepl :nrepl-host])
-                                 :port (db/get-in project [:current-nrepl :nrepl-port]))]
-    ;; TODO Improve this timeout, what will happen for tests/evals
-    ;; taking more than this timeout? should we really fail?
-    (-> (nrepl.core/client conn 60000)
-        (nrepl.core/message message)
-        doall
-        nrepl.core/combine-responses)))
+  (when (config/nrepl-debug?)
+    (logger/info "Requesting:" message))
+  (let [response (with-open [conn ^FnTransport (nrepl.core/connect
+                                                :host (db/get-in project [:current-nrepl :nrepl-host])
+                                                :port (db/get-in project [:current-nrepl :nrepl-port]))]
+                   ;; TODO Improve this timeout, what will happen for tests/evals
+                   ;; taking more than this timeout? should we really fail?
+                   (-> (nrepl.core/client conn 60000)
+                       (nrepl.core/message message)
+                       doall
+                       nrepl.core/combine-responses))]
+    (when (config/nrepl-debug?)
+      (logger/info "Responded:" response))
+    response))
 
 (defn eval [& {:keys [^Project project ns code]
                :or {ns (or (db/get-in project [:current-nrepl :ns]) "user")}}]
@@ -48,10 +55,16 @@
   (send-message project {:op "describe"}))
 
 (defn sym-info [^Project project ns sym]
-  (send-message project {:op "info" :ns ns :sym sym}))
+  (send-message project {:op "info" :ns ns :sym sym :session (db/get-in project [:current-nrepl :session-id])}))
 
 (defn run-tests [^Project project {:keys [ns tests on-ns-not-found on-out on-err on-succeeded on-failed]}]
-  (let [{:keys [summary results status out err] :as response} (send-message project {:op "test" :ns ns :tests (when (seq tests) tests)})]
+  (let [{:keys [summary results status out err] :as response}
+        (send-message project {:op "test"
+                               :load? (str (boolean ns))
+                               :session (db/get-in project [:current-nrepl :session-id])
+                               :ns ns
+                               :tests (when (seq tests) tests)
+                               :fail-fast  "true"})]
     (when (some #(= % "namespace-not-found") status)
       (on-ns-not-found ns))
     (when out (on-out out))
