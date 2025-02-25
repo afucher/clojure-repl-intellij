80a7fc098378cc887ae3c839053c0d041e97b640
Eric Dallo <ericdallo06@hotmail.com>
2025-02-25 09:45:28 -0300

    Fix missing text attrs on color settings page

--- src/main/clojure/com/github/clojure_repl/intellij/extension/color_settings_page.clj
+++ src/main/clojure/com/github/clojure_repl/intellij/extension/color_settings_page.clj
@@ -3,6 +3,7 @@
    :name com.github.clojure_repl.intellij.extension.ColorSettingsPage
    :implements [com.intellij.openapi.options.colors.ColorSettingsPage])
   (:require
+   [clojure.string :as string]
    [com.github.clojure-repl.intellij.ui.color :as ui.color])
   (:import
    [com.intellij.lang Language]
@@ -14,17 +15,20 @@
 
 (defn -getDisplayName [_] "Clojure REPL")
 
-(def ^:private title-by-text-attribute
-  {:repl-window "REPL window"
-   :eval-inline-hint "Eval inline inlay hint"
-   :test-summary-label "Test summary//Label"
-   :test-result-error "Test summary//Error"
-   :test-result-fail "Test summary//Fail"
-   :test-result-pass "Test summary//Pass"})
+(defn ^:private title-by-text-attribute []
+  (merge (reduce (fn [m k]
+                   (let [display-name (-> (name k)
+                                          (string/replace "--" "//")
+                                          (string/replace "-" " ")
+                                          string/capitalize)]
+                     (assoc m k display-name)))
+                 {}
+                 (keys (ui.color/text-attributes)))
+         {:repl-window "REPL window"}))
 
 (defn -getAttributeDescriptors [_]
   (->> (ui.color/text-attributes)
-       (mapv #(AttributesDescriptor. ^String (title-by-text-attribute (first %)) ^TextAttributesKey (second %)))
+       (mapv #(AttributesDescriptor. ^String ((title-by-text-attribute) (first %)) ^TextAttributesKey (second %)))
        (into-array AttributesDescriptor)))
 
 (defn -getColorDescriptors [_] ColorDescriptor/EMPTY_ARRAY)
