03cf5fce88ab7be903c0c0cea6d09a5571924dbc
André Camargo <andre.camargo@nubank.com.br>
2025-03-12 11:56:12 -0300

    Attempt to improve the test

--- src/test/clojure/com/github/clojure_repl/intellij/foo_test.clj
+++ src/test/clojure/com/github/clojure_repl/intellij/foo_test.clj
@@ -1,6 +1,7 @@
 (ns com.github.clojure-repl.intellij.foo-test
   (:require
-   [clojure.test :refer [deftest is]])
+   [clojure.test :refer [deftest is]]
+   [com.github.ericdallo.clj4intellij.app-manager :as app-manager])
   (:import
    [com.intellij.testFramework LightProjectDescriptor]
    [com.intellij.testFramework.fixtures IdeaTestFixtureFactory]))
@@ -12,8 +13,12 @@
         fixture (-> factory
                     (.createLightFixtureBuilder LightProjectDescriptor/EMPTY_PROJECT_DESCRIPTOR "fooo")
                     (.getFixture))
-        fixture-2 (.createCodeInsightFixture factory fixture)]
-    (.setUp fixture-2))
+        fixture-2 (.createCodeInsightFixture factory fixture)
+        _ (.setUp fixture-2)
+        virtual-file (.createFile fixture-2 "deps.edn" "{}")]
+       (is (.getProject fixture-2))
+       (is virtual-file)
+       (app-manager/write-action! {:run-fn (fn [] (.openFileInEditor fixture-2 virtual-file))}))
   (println "-------------------------------------------->")
 
   (is true))
