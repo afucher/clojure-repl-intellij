c0f37672d6a6ff12b1924d94d5801afff4fc0492
Eric Dallo <ericdallo06@hotmail.com>
2025-03-18 11:24:13 -0300

    indent

--- src/main/clojure/com/github/clojure_repl/intellij/extension/namespace_search_contributor.clj
+++
@@ -1,4 +0,0 @@
-(ns com.github.clojure-repl.intellij.extension.namespace-search-contributor
-  (:gen-class
-   :name com.github.clojure_repl.intellij.extension.NamespaceSearchContributor
-   :implements [com.intellij.ide.actions.searcheverywhere.SearchEverywhereContributor]))
--- src/test/clojure/com/github/clojure_repl/intellij/foo_test.clj
+++ src/test/clojure/com/github/clojure_repl/intellij/foo_test.clj
@@ -1,53 +1,53 @@
 (ns com.github.clojure-repl.intellij.foo-test
-    (:require
-     [clojure.test :refer [deftest is]]
-     [com.github.clojure-repl.intellij.configuration.factory.local :as config.factory.local]
-     [com.github.clojure-repl.intellij.db :as db])
-    (:import
-     [com.github.clojure_repl.intellij.configuration ReplRunConfigurationType]
-     [com.intellij.execution ProgramRunnerUtil RunManager]
-     [com.intellij.execution.executors DefaultRunExecutor]
-     [com.intellij.execution.ui RunContentManager]
-     [com.intellij.openapi.command WriteCommandAction]
-     [com.intellij.testFramework LightProjectDescriptor]
-     [com.intellij.testFramework.fixtures CodeInsightTestFixture IdeaTestFixtureFactory]
-     [com.intellij.util ThrowableRunnable]))
+  (:require
+   [clojure.test :refer [deftest is]]
+   [com.github.clojure-repl.intellij.configuration.factory.local :as config.factory.local]
+   [com.github.clojure-repl.intellij.db :as db])
+  (:import
+   [com.github.clojure_repl.intellij.configuration ReplRunConfigurationType]
+   [com.intellij.execution ProgramRunnerUtil RunManager]
+   [com.intellij.execution.executors DefaultRunExecutor]
+   [com.intellij.execution.ui RunContentManager]
+   [com.intellij.openapi.command WriteCommandAction]
+   [com.intellij.testFramework LightProjectDescriptor]
+   [com.intellij.testFramework.fixtures CodeInsightTestFixture IdeaTestFixtureFactory]
+   [com.intellij.util ThrowableRunnable]))
 
 (set! *warn-on-reflection* true)
 
 ;; TODO migrate these functions to clj4intellij app-manager
 (defn write-command-action [project run-fn]
-      (.run (WriteCommandAction/writeCommandAction project)
-            (reify ThrowableRunnable
-                   (run [_]
-                        (run-fn)))))
+  (.run (WriteCommandAction/writeCommandAction project)
+        (reify ThrowableRunnable
+          (run [_]
+            (run-fn)))))
 
 (defn setup ^CodeInsightTestFixture []
-      (let [factory (IdeaTestFixtureFactory/getFixtureFactory)
-            raw-fixture (-> factory
-                            (.createLightFixtureBuilder LightProjectDescriptor/EMPTY_PROJECT_DESCRIPTOR (str *ns*))
-                            (.getFixture))
-            fixture (.createCodeInsightFixture factory raw-fixture)]
-           (.setUp fixture)
-           (is (.getProject fixture))
-           fixture))
+  (let [factory (IdeaTestFixtureFactory/getFixtureFactory)
+        raw-fixture (-> factory
+                        (.createLightFixtureBuilder LightProjectDescriptor/EMPTY_PROJECT_DESCRIPTOR (str *ns*))
+                        (.getFixture))
+        fixture (.createCodeInsightFixture factory raw-fixture)]
+    (.setUp fixture)
+    (is (.getProject fixture))
+    fixture))
 
 (deftest foo-test
-         (let [fixture (setup)
-               deps-file (.createFile fixture "deps.edn" "{}")]
-              (is deps-file)
-              (write-command-action
-               (.getProject fixture)
-               (fn [] (.openFileInEditor fixture deps-file)))
-              (is (.getEditor fixture))
-              (let [project (.getProject fixture)
-                    run-manager (RunManager/getInstance project)
-                    configuration (config.factory.local/configuration-factory (ReplRunConfigurationType.))
-                    configuration-instance (.createConfiguration run-manager "Local REPL" configuration)]
-                   (ProgramRunnerUtil/executeConfiguration configuration-instance
-                                                           (DefaultRunExecutor/getRunExecutorInstance))
-                   (Thread/sleep 2000)
-                   (println '_______________ (.getExitCode (.getProcessHandler (.getSelectedContent (RunContentManager/getInstance project)))))
-                   (clojure.pprint/pprint @db/db*)
+  (let [fixture (setup)
+        deps-file (.createFile fixture "deps.edn" "{}")]
+    (is deps-file)
+    (write-command-action
+     (.getProject fixture)
+     (fn [] (.openFileInEditor fixture deps-file)))
+    (is (.getEditor fixture))
+    (let [project (.getProject fixture)
+          run-manager (RunManager/getInstance project)
+          configuration (config.factory.local/configuration-factory (ReplRunConfigurationType.))
+          configuration-instance (.createConfiguration run-manager "Local REPL" configuration)]
+      (ProgramRunnerUtil/executeConfiguration configuration-instance
+                                              (DefaultRunExecutor/getRunExecutorInstance))
+      ;; (Thread/sleep 2000)
+      ;; (println '_______________ (.getExitCode (.getProcessHandler (.getSelectedContent (RunContentManager/getInstance project)))))
+      (clojure.pprint/pprint @db/db*)
 
-                   #_())))
+      #_())))
